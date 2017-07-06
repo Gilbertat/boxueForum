38cd716967262f4489430e1cbb9ea3a94284f1ca
Gilbertat <shiyue45457@gmail.com>
2017-07-06 16:17:52 +0800

    use highlight.js cdn

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -9,7 +9,7 @@
     <title>@yield('title', 'boxue') - {{env('APP_NAME')}} </title>
     <link rel="stylesheet" href="{{mix('css/app.css')}}">
     <link rel="stylesheet" href="{{asset('css/nprogress.css')}}">
-    <link rel="stylesheet" href="{{asset('css/highlight.css')}}">
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/styles/default.min.css">
     <script>
         window.Laravel = {!! json_encode([
             'csrfToken' => csrf_token(),
@@ -26,8 +26,9 @@
     </div>
 </div>
 <script src="{{asset('js/nprogress.js')}}"></script>
-<script src="{{asset('js/highlight.js')}}"></script>
 <script src="{{mix('js/app.js')}}"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/highlight.min.js"></script>
+
 <!-- JS Customization -->
 @yield('script')
 </body>
